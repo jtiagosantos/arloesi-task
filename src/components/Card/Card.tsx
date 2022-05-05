cada3a39759dda9ed361b1053a2816bbb40fe746
jtiagosantos <tiagosan040@gmail.com>
2022-05-05 19:29:18 -0400

    feat: 🎸 modify component to open modal when click in trash icon

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,16 +5,21 @@ import { FC } from 'react';
 interface CardProps {
   taskName: string;
   taskDescription: string;
+  onOpenModal: () => void;
 }
 
-export const Card: FC<CardProps> = ({ taskName, taskDescription }) => {
+export const Card: FC<CardProps> = ({
+  taskName,
+  taskDescription,
+  onOpenModal,
+}) => {
   return (
     <Container>
       <div>
         <h1>{taskName}</h1>
         <p>{taskDescription}</p>
       </div>
-      <button>
+      <button onClick={onOpenModal}>
         <img src={trashIcon} alt="Trash icon" />
       </button>
     </Container>
