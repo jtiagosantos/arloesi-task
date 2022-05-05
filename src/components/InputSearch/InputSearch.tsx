9cab4d2e6c7e03a7fb219dae3c46eeba4cfeb18d
jtiagosantos <tiagosan040@gmail.com>
2022-05-05 15:12:29 -0400

    fix: 🐛 change placeholder text

--- src/components/InputSearch/InputSearch.tsx
+++ src/components/InputSearch/InputSearch.tsx
@@ -4,7 +4,7 @@ import { Container } from '@/components/InputSearch/styles';
 export const InputSearch = () => {
   return (
     <Container>
-      <input type="text" placeholder="text" />
+      <input type="text" placeholder="Search" />
       <img src={searchIcon} alt="Search icon" />
     </Container>
   );
